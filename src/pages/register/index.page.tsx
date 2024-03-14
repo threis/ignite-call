ecc5da98af8966ea04660e42c85b0b85abe182b0
Thiago <thiagoreis93@gmail.com>
2024-03-14 17:44:13 -0300

    add users api route

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -40,8 +42,19 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+  async function handleRegister({ username, name }: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        username,
+        name,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err?.response?.data?.message)
+      } else {
+        console.error(err)
+      }
+    }
   }
 
   return (
